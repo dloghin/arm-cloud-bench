623c03e351a53feeec0be960aacc6260f3932e84
Ubuntu <ubuntu@ip-172-31-13-47.us-west-2.compute.internal>
2022-10-05 13:53:04 +0000

    add prepare script for ycsb

---
+++ ycsb/prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
+tar xfvz ycsb-0.17.0.tar.gz
+cd ycsb-0.17.0
+
+sudo apt -y install redis-server memcached python2 openjdk-8-jdk
