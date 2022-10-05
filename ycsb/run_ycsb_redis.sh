c3703befcd6eeb4561d205f2f70ebc46da9e1d2c
Dumi Loghin <dumiloghin@gmail.com>
2022-10-05 05:26:09 +0000

    support memcached

--- ycsb/run_ycsb_redis.sh
+++ ycsb/run_ycsb_redis.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-THREADS=20
+set -x 
 
-BASECMD1="python2 ycsb-0.17.0/bin/ycsb load redis -threads $THREADS -s"
-BASECMD2="python2 ycsb-0.17.0/bin/ycsb run redis -threads $THREADS -s"
-CMDSUFFIX="-p redis.host=127.0.0.1 -p redis.port=6379"
+# DB="redis"
+DB="memcached"
+
+THREADS=1
+
+BASECMD1="python2 ycsb-0.17.0/bin/ycsb load $DB -threads $THREADS -s"
+BASECMD2="python2 ycsb-0.17.0/bin/ycsb run $DB -threads $THREADS -s"
+
+if [ "$DB" == "redis" ]; then
+	CMDSUFFIX="-p redis.host=127.0.0.1 -p redis.port=6379"
+	CMDCLEAN="redis-cli flushall"
+elif [ "$DB" == "memcached" ]; then
+	CMDSUFFIX="-p memcached.hosts=127.0.0.1"
+	CMDCLEAN="echo 'flush_all' | netcat -q 1 localhost 11211"
+fi
 
 REPS=3
 DISTRS="uniform zipfian"
 WORKLOADS="workloada workloadb workloadc"
 
 TSTP=`date +%F-%H-%M-%S`
-LOGD="logs-$TSTP"
+LOGD="logs-$DB-$THREADS-threads-$TSTP"
 mkdir $LOGD
 
 set -x
@@ -19,7 +31,7 @@ set -x
 for REP in `seq 1 $REPS`; do
 	for DISTR in $DISTRS; do
 		for WORKLOAD in $WORKLOADS; do
-			redis-cli flushall
+			$CMDCLEAN
 			sleep 3
 			$BASECMD1 -P workloads_$DISTR/$WORKLOAD $CMDSUFFIX | tee $LOGD/log-load-$REP-$DISTR-$WORKLOAD.txt
 			$BASECMD2 -P workloads_$DISTR/$WORKLOAD $CMDSUFFIX | tee $LOGD/log-run-$REP-$DISTR-$WORKLOAD.txt
