ba6f170c4d1717745c6b1dd37105e42124789d73
Dumi Loghin <dumiloghin@gmail.com>
2022-11-07 10:11:32 +0000

    some changes to TPCC

--- tpcc/run_tpcc.sh
+++ tpcc/run_tpcc.sh
@@ -24,11 +24,15 @@ REPS=3
 
 CONNS="1 4 8 10 16 20 24 30"
 
+WAREHOUSES=1000
+TIME_WARMUP=10
+TIME_BENCHMARK=60
+
 # prepare
 $PREP_CMD < prepare.sql
 $RUN_CMD < $TPCC_HOME/create_table.sql
 $RUN_CMD < $TPCC_HOME/add_fkey_idx.sql
-$TPCC_HOME/tpcc_load -h$HOST -dtpcc -utest -pTest1234 -w 10
+$TPCC_HOME/tpcc_load -h$HOST -dtpcc -utest -pTest1234 -w $WAREHOUSES
 
 TSTP=`date +%F-%H-%M-%S`
 LOGD="logs-tpcc-$TSTP"
@@ -37,7 +41,7 @@ mkdir $LOGD
 # run
 for REP in `seq 1 $REPS`; do
 	for CONN in $CONNS; do
-		$TPCC_HOME/tpcc_start -h$HOST -P3306 -dtpcc -utest -pTest1234 -w10 -c$CONN -r10 -l60 | tee $LOGD/log-tpcc-$CONN-$REP.txt
+		$TPCC_HOME/tpcc_start -h$HOST -P3306 -dtpcc -utest -pTest1234 -w$WAREHOUSES -c$CONN -r$TIME_WARMUP -l$TIME_BENCHMARK | tee $LOGD/log-tpcc-$CONN-$REP.txt
 	done
 done
 tar cjf $LOGD.tar.bz2 $LOGD
